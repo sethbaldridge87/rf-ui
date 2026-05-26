124e4f904622ca008cd7d968d8ffd9d726307962
sethbaldridge87 <sethbaldridge87@gmail.com>
2026-05-25 18:15:44 -0600

    README file updated, and homepage intro text added.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,15 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <section><h2>This is the Home Page</h2></section>
+    <section>
+      <h2>Dear RainFocus engineering team,</h2>
+      <p>Welcome to my UI submission! This layout should adhere to the guidelines in the Figma mockup perfectly!</p> 
+      <p>Loading and 404 pages are also included.</p>
+      <p>It is responsive across all screen sizes down to 320px, and the nav and meganav are fully functional.</p>
+      <p>I improvised the mobile layout, so please feel free to check that out as well.</p>
+      <p>Also, I'd like you to pay particular attention to this UI's complete ADA compliance, including appropriate use of aria labels and tab navigation. Try navigating through the layout using just your keyboard!</p>
+      <p>The complete requested layout is located <Link href="attendees/attendees">here</Link></p>
+    </section>
   );
 }
