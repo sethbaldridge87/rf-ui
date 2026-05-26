6b257b76d5223f6b34d7151d64e5c9eda3da1df2
sethbaldridge87 <sethbaldridge87@gmail.com>
2026-05-25 18:31:00 -0600

    Extra notes for the home page, and minor outline focus adjustment.

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,18 @@ import Link from "next/link";
 export default function Home() {
   return (
     <section>
-      <h2>Dear RainFocus engineering team,</h2>
-      <p>Welcome to my UI submission! This layout should adhere to the guidelines in the Figma mockup perfectly!</p> 
+      <h2>Dear RainFocus Engineering Team,</h2>
+      <p>Welcome to my UI submission! This layout should adhere to the guidelines in the Figma mockup perfectly!</p>
       <p>Loading and 404 pages are also included.</p>
       <p>It is responsive across all screen sizes down to 320px, and the nav and meganav are fully functional.</p>
       <p>I improvised the mobile layout, so please feel free to check that out as well.</p>
       <p>Also, I'd like you to pay particular attention to this UI's complete ADA compliance, including appropriate use of aria labels and tab navigation. Try navigating through the layout using just your keyboard!</p>
-      <p>The complete requested layout is located <Link href="attendees/attendees">here</Link></p>
+      <p>This is just the homepage, but the complete requested layout is located <Link href="attendees/attendees">here</Link></p>
+      <p>Please let me know if you have any questions or feedback!</p>
+      <br />
+      <p>All the best,</p>
+      <p>Seth Baldridge</p>
+      <a href="https://github.com/sethbaldridge87/rf-ui" target="blank">Source code</a>
     </section>
   );
 }
