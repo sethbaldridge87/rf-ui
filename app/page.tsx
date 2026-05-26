f982eb995413466b6e8217eaba8127c5ce3fdf80
sethbaldridge87 <sethbaldridge87@gmail.com>
2026-05-25 18:32:22 -0600

    Punctuation.

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       <p>It is responsive across all screen sizes down to 320px, and the nav and meganav are fully functional.</p>
       <p>I improvised the mobile layout, so please feel free to check that out as well.</p>
       <p>Also, I'd like you to pay particular attention to this UI's complete ADA compliance, including appropriate use of aria labels and tab navigation. Try navigating through the layout using just your keyboard!</p>
-      <p>This is just the homepage, but the complete requested layout is located <Link href="attendees/attendees">here</Link></p>
+      <p>This is just the homepage, but the complete requested layout is located <Link href="attendees/attendees">here.</Link></p>
       <p>Please let me know if you have any questions or feedback!</p>
       <br />
       <p>All the best,</p>
