7b2690ce7599ed8c5fedb5877b0ffb12c51620be
sethbaldridge87 <sethbaldridge87@gmail.com>
2026-08-13 17:44:10 -0600

    Adding some accessible text to the loading state.

--- app/loading.tsx
+++ app/loading.tsx
@@ -3,6 +3,7 @@ import styles from './loading.module.scss';
 export default function Loading() {
   return (
     <section role="status" className={styles.loading}>
+      <p>Loading</p>
       <div aria-hidden="true" className={styles.loadBubbles}>
         <span></span>
         <span></span>
